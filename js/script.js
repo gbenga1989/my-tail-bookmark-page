13352de3f30510ae3f1a16fee58c417c84cf7497
gbenga <odetoyinbog@gmail.com>
2025-03-11 12:08:23 +0100

    11-2

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ const panels = document.querySelectorAll('.panel')
 
 // tabs menu event listener
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
+
+
+function onTabClick(e){
+  tabs.forEach((tab) => {
+    tab.children[0]
+  })
+}
