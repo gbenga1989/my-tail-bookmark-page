427c39eb5002765e428df162936bea1ec22b0eae
gbenga <odetoyinbog@gmail.com>
2025-03-11 12:16:36 +0100

    11-3

--- js/script.js
+++ js/script.js
@@ -6,7 +6,17 @@ tabs.forEach((tab) => tab.addEventListener('click', onTabClick))
 
 
 function onTabClick(e){
+  // deactivate all tabs
   tabs.forEach((tab) => {
-    tab.children[0]
+    tab.children[0].classList.remove('border-softRed',
+      'border-b-4',
+      'md:border-b-0'
+    )
   })
+
+  // hide all panels
+  panels.forEach((panel) => panel.classList.add('hidden'))
+
+  // activate a new tab and panel based on the target
+  e.target.classList.add('border-softRed', 'border-b-4')
 }
